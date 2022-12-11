028015feebe2d09a2fc7cdad616039e9a497bce7
Archery Luna <76516425+ArcheryLuna@users.noreply.github.com>
2022-12-11 11:32:09 +0000

    Day 8 Begining

---
+++ Day 8/index.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Day8 {
+    static void PartOne(dynamic input) {
+
+    }
+    public static void Main() {
+        var input = System.IO.File.ReadAllLines(@"input.txt");
+        PartOne(input);
+    }
+}
